89d5139824d17a50ef10f1574e937841f56062b6
GreatMen456 <harchuk-denis@hotmail.com>
2018-07-25 23:00:04 +0300

    Bug fixed

--- models/model.js
+++ models/model.js
@@ -3,7 +3,6 @@ const {
    stroyashchiyesya, types, klasses, roommate, rassrochka, remont
 } = require('../helpers/data');
 
-
 module.exports = function regular(url) {
   function joining(arg) {
     return arg.join('|');
@@ -16,43 +15,97 @@ module.exports = function regular(url) {
       return  '';
     }
   };
+
   function klass_check(reg) {
     if (reg != null && reg != 'undefined') {
       return reg[0].replace(/-klassa/g, '');
-      //console.log(reg);
     } else {
       return  '';
-      //console.log(reg);
+    }
+  };
+
+  function metro_check(reg) {
+    if (reg != null && reg != 'undefined') {
+      return reg[0].replace(/-metro-/g, '');
+    } else {
+      return  '';
+    }
+  };
+
+  function street_check(reg) {
+    if (reg != null && reg != 'undefined') {
+      return reg[0].replace(/-ulitsa-/g, '');
+    } else {
+      return  '';
+    }
+  };
+
+  function area_check(reg) {
+    if (reg != null && reg != 'undefined') {
+      return reg[0].replace(/-rayon-/g, '');
+    } else {
+      return  '';
     }
   }
 
-  var language = new RegExp(`(${joining(languages)})`).exec(url),
-      region = new RegExp(`(${joining(regions)}|[a-z]+)(?=-oblast)`).exec(url),
+  var language = new RegExp(`(${joining(languages)}+?)`).exec(url),
+      region = new RegExp(`(${joining(regions)}+?)(?=-oblast)`).exec(url),
       sity = new RegExp(`(${joining(sities)})(?!(aya|skaya)-oblast)`).exec(url),
-      area = new RegExp(`(-rayon-|'')(=?([a-z]+))`).exec(url),
-      metro_reg = new RegExp(`(-metro-|'')(=?([a-z]+))`).exec(url),
-      bank = new RegExp(`(${joining(banks)})(?=-bereg)`).exec(url),
+      area = new RegExp(`(-rayon-)(=?(\\w+))`).exec(url),
+      metro_reg = new RegExp(`(-metro-)(=?(\\w+))`).exec(url),
+      bank = new RegExp(`(${joining(banks)}+?)(?=-bereg)`).exec(url),
       near_metro_reg = new RegExp(`(${joining(near_metro)})`, 'i').exec(url),
-      street = new RegExp(`(ulitsa-)(=?([a-z]+))`).exec(url),
+      street = new RegExp(`(-ulitsa-)(=?(\\w+))`).exec(url),
       sdannyye_reg = new RegExp(`(${joining(sdannyye)})`).exec(url),
       building = new RegExp(`(${joining(stroyashchiyesya)})`).exec(url),
       type = new RegExp(`(${joining(types)})`).exec(url),
       klass = new RegExp(`(${joining(klasses)})`).exec(url),
       roommate_reg = new RegExp(`(${joining(roommate)})`).exec(url),
       rassrochka_reg = new RegExp(`(${joining(rassrochka)})`).exec(url),
-      remont_reg = new RegExp(`(${joining(remont)})`).exec(url),
-      result_reg = [
-        url,check(language, 0),check(region, 0),check(sity, 0),
-        check(area, 2),check(metro_reg, 2),check(bank, 0),check(near_metro_reg, 0),check(street, 2),
-        check(sdannyye_reg, 0),check(building, 0),check(type, 0),klass_check(klass),
-        check(roommate_reg, 0),check(rassrochka_reg, 0),check(remont_reg, 0)
-      ];
+      remont_reg = new RegExp(`(${joining(remont)})`, 'i').exec(url);
 
-  for (var i = 0; i < result_reg.length; i++) {
-    if (result_reg[i] != '') {
-      console.log(result_reg[i])
-    } else {
-      continue;
-    }
+
+      var reg_ex = new RegExp(`(${
+        check(language, 0)
+      })\\S*?(${
+        check(region, 0)
+      })\\S*?(${
+        check(sity, 0)
+      })\\S*?(${
+        area_check(area)
+      })\\S*?(${
+        metro_check(metro_reg)
+      })\\S*?(${
+        check(bank, 0)
+      })\\S*?(${
+        check(near_metro_reg, 0)
+      })\\S*?(${
+        street_check(street)
+      })\\S*?(${
+        check(sdannyye_reg, 0)
+      })\\S*?(${
+        check(building, 0)
+      })\\S*?(${
+        check(type, 0)
+      })\\S*?(${
+        klass_check(klass)
+      })\\S*?(${
+        check(roommate_reg, 0)
+      })\\S*?(${
+        check(rassrochka_reg, 0)
+      })\\S*?(${
+        check(remont_reg, 0)
+      })`,'gim').exec(url);
+
+    if (reg_ex != null) {
+      for (var i = 0; i < reg_ex.length; i++) {
+        if (reg_ex[i] != '') {
+          console.log(reg_ex[i])
+        } else {
+          continue;
+        }
+      }
+  } else {
+    console.log('ERROR!!!');
   }
 }
